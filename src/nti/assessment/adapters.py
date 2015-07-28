76184d4792bebea26f11f342f3ec10f2db3269f7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-28 15:17:11 +0000

    don't sort response
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@69607 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/adapters.py
+++ src/nti/assessment/adapters.py
@@ -63,7 +63,7 @@ class MultipleChoiceMultipleAnswerPartResponseNormalizer(AbstractResponseNormali
 
 	def __call__(self):
 		if self.response.value:
-			result = tuple(sorted(self.response.value))
+			result = tuple(self.response.value) # Sorted?
 		else:
 			result = ()
 		return result
